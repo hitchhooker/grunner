9e45e2e01c2db6f5fbe24e6a1ab656ca7a100712
hitchhooker <tommi@niemi.lol>
2023-03-14 08:29:59 +0200

    make list

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -8,13 +8,15 @@ interface NavItemProps {
 
 const NavItem: Component<NavItemProps> = (props) => {
   return (
-    <a
-      href={props.href}
-      class="block mt-4 md:inline-block md:mt-0 hover:text-green-600 mr-4"
-    >
-      {props.icon}
-      {props.text}
-    </a>
+    <li>
+      <a
+        href={props.href}
+        class="block mt-4 md:inline-block md:mt-0 hover:text-green-600 mr-4"
+      >
+        {props.icon}
+        {props.text}
+      </a>
+    </li>
   );
 };
 
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,19 +6,19 @@ import LogoutButton from "./LogoutButton";
 const Navigation: Component = () => {
   return (
     <nav
-      class="bg-#EFAAAA text-black flex items-center justify-between flex-wrap p-6 md:w-64 md:flex-col"
+      class="bg-#EFAAAA md:h-screen text-black flex items-center justify-between flex-wrap p-6 md:w-64 md:flex-col"
     >
       <div class="flex items-center flex-row mr-6 p-0 md:p-4">
         <i class="i-carbon-logo-google text-2xl text-#4285F4"></i>
         <span class="font-bold text-xl tracking-tight">runner</span>
       </div>
       <MenuButton />
-      <div class="text-sm md:flex-grow  md:items-center md:text-md">
+      <ul class="text-sm md:flex-grow  md:items-center md:text-md">
         <NavItem href="#" icon={<i class="i-mdi-light-view-dashboard text-lg"></i>} text="Dashboard" />
         <NavItem href="#" icon={<i class="i-healthicons-lymph-nodes-outline text-lg"></i>} text="Deploy" />
         <NavItem href="#" icon={<i class="i-mdi-light-chart-areaspline text-lg"></i>} text="Monitoring" />
         <NavItem href="#" icon={<i class="i-mdi-light-settings text-lg"></i>} text="Settings" />
-      </div>
+      </ul>
       <div>
         <LogoutButton />
       </div>
